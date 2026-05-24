b850c1eac8db3c93202abad62dd26f552df8a871
bartur8195 <bartur8195@gmail.com>
2026-05-24 13:01:19 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+a = float(input("1-son: "))
+b = float(input("2-son: "))
+amal = input("Amal (+, -, *, /): ")
+
+if amal == "+":
+    print(a + b)
+elif amal == "-":
+    print(a - b)
+elif amal == "*":
+    print(a * b)
+elif amal == "/":
+    print(a / b)
+else:
+    print("Noto'g'ri amal")
